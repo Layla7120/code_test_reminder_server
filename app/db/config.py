1d9fa088222005d7fb394b20ab77b2f6ad7432be
Layla Oh <crispylemon7120@gmail.com>
2024-12-10 11:22:02 +0900

    chore(deps): update requirements.txt
    - 최신 라이브러리 버전으로 dependencies 업데이트
    - 호환성 및 보안 개선

--- app/db/config.py
+++ app/db/config.py
@@ -7,5 +7,5 @@ class Config:
         'database': 'code_test_app'
     }
     SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{db['user']}:{db['password']}@" \
-             f"{db['host']}:{db['port']}/{db['database']}?charset=utf8"
+             f"{db['host']}:{db['port']}/{db['database']}?charset=utf8mb4&collation=utf8mb4_general_ci"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
